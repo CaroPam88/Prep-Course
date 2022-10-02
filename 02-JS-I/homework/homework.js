7043961429ff1472aa5f3f10b8150020dc8a32c1
Carolina Ramirez <solcaro_8717@hotmail.com>
2022-10-02 12:28:01 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -3,7 +3,7 @@
 const henryReadingTime = require("henry-reading-time");
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Estudiante";
+const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 1;
